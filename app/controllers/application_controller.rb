b7cab49a36125d3e9ad32b5f2cd1743d2a64fc65
Lolisuki18 <leninh2004@gmail.com>
2025-09-19 19:41:20 +0700

    Disable CSRF for API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
   end
+
+  #ta có Rails tự động bật CSRF protection
+  #nếu muốn tắt thì dùng dòng dưới
+  # Thêm dòng này để disable CSRF cho API
+  protect_from_forgery with: :null_session
 end
